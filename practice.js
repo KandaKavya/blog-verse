a08fc053dcd458ca471d33f5d97bde9d86d2e145
KandaKavya <kavyakanda18@gmail.com>
2025-09-06 12:33:34 +0530

    Learned datatypes in js

--- practice.js
+++ practice.js
@@ -1,1 +1,33 @@
-console.log("Hello World! Kavyasri")
+console.log("Hello World! Kavyasri");
+
+let name ="Dev Astra";
+console.log(name);
+
+let num =18;
+console.log(num);
+
+let isLogged= false;
+console.log(isLogged)
+
+let x;
+console.log(x)
+
+let y=null;
+console.log(y)
+
+console.log(typeof name)
+console.log(typeof num)
+console.log(typeof isLogged)
+console.log(typeof x)
+
+let arr=[1,2,3,4,5];
+console.log(arr[0])
+console.log(typeof arr)
+
+let person = {
+    name:"kavya",
+    age:19
+}
+console.log(person.name)
+console.log(person.age)
+console.log(typeof person)
